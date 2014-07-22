2c479fa933be9338c1b61428717cb380afa5cb52
Andrew Rea <andrew.rea@hp.com>
2014-07-22 10:49:42 +0100

    remove an item from the beginning

--- javascript/linked_list.js
+++ javascript/linked_list.js
@@ -8,6 +8,10 @@ function Node(item) {
     next = node;
   }
 
+  function getNext(){
+    return next;
+  }
+
   function accept(visitor) {
     visitor.visit(item)
     if (next !== undefined && next !== null) {
@@ -18,6 +22,7 @@ function Node(item) {
   return {
     item: item,
     setNext: setNext,
+    getNext : getNext,
     accept: accept
   };
 
@@ -43,6 +48,14 @@ function StringVisitor() {
 
 describe('Linked List', function() {
 
+  var visitor;
+  var chain;
+
+  beforeEach(function(){
+    visitor = new StringVisitor();
+    chain = createChain();
+  });
+
   function createChain(){
     var first = new Node('A');
     var second = new Node('B');
@@ -53,20 +66,22 @@ describe('Linked List', function() {
   }
 
   it('creating a list', function() {
-    var visitor = new StringVisitor();
-    var chain = createChain();
     chain.accept(visitor);
     assert.ok(visitor.hasResult('ABC'));
   });
 
   it('insert at the beginning', function(){
-    var visitor = new StringVisitor();
-    var chain = createChain();
     var oldFirst = chain;
     var first = new Node('Z');
     first.setNext(oldFirst);
     first.accept(visitor);
     assert.ok(visitor.hasResult('ZABC'));
+  });
+
+  it('remove item from the beginning',function(){
+    var first = chain.getNext();
+    first.accept(visitor);
+    assert.ok(visitor.hasResult('BC'));
   });
 
 });
